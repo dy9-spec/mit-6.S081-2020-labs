ea34d7234c9b91c3eb4e19189e22481c27b3ebb6
dy9-spec <di.yao.think@gmail.com>
2021-04-04 16:49:46 -0700

    lab04-q01 & q02

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,21 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void backtrace()
+{
+    printf("backtrace:\n");
+    uint64 cur_fp = r_fp();
+    uint64 top_stack = PGROUNDUP(cur_fp);
+    while (cur_fp < top_stack) {
+        /**
+         * cur_fp - constant is of int type
+         * firstly convert that into an address by (uint64 *)
+         * then retrieve the content stored at that address with the operator *
+         * */
+        uint64 cur_res = *(uint64 *)(cur_fp - 8);
+        printf("%p\n", cur_res);
+        cur_fp = *(uint64 *)(cur_fp - 16);
+    }
 }
